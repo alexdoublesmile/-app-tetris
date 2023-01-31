565e07d31d85e6b3feac1f64d21e53af2e0bf757
Александр Плохой <alexplohoy@mail.ru>
2023-01-31 14:15:50 +0300

    add grid params

--- src/main/java/com/joyful/tetris/GameArea.java
+++ src/main/java/com/joyful/tetris/GameArea.java
@@ -4,11 +4,17 @@ import java.awt.Graphics;
 import javax.swing.JPanel;
 
 public class GameArea extends JPanel { 
+    private final int gridCellSize;
+    private final int gridRows;
+    private final int gridColumns;
+    
     public GameArea(JPanel placeholder) {
         placeholder.setVisible(false);
         setBounds(placeholder.getBounds());
         setBackground(placeholder.getBackground());
         setBorder(placeholder.getBorder());
+        
+        gridColumns = 
     }
     
     @Override
