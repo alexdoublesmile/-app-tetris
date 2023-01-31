b819409a188a145bd75ed7b379313ab3473e034a
Александр Плохой <alexplohoy@mail.ru>
2023-01-31 14:54:18 +0300

    add block accessors

--- src/main/java/com/joyful/tetris/TetrisBlock.java
+++ src/main/java/com/joyful/tetris/TetrisBlock.java
@@ -1,5 +1,21 @@
 package com.joyful.tetris;
 
-public class TetrisBlock {
+import java.awt.Color;
 
+public class TetrisBlock {
+    private int[][] shape;
+    private Color color;
+
+    public TetrisBlock(int[][] shape, Color color) {
+        this.shape = shape;
+        this.color = color;
+    }
+
+    public int[][] getShape() {
+        return shape;
+    }
+
+    public Color getColor() {
+        return color;
+    }
 }
