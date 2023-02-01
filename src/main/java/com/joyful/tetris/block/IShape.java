18b6e519d8480a0a25eec2a174800ae2ce9d1452
Александр Плохой <alexplohoy@mail.ru>
2023-02-01 12:05:04 +0300

    fix ishape rotation

--- src/main/java/com/joyful/tetris/block/IShape.java
+++ src/main/java/com/joyful/tetris/block/IShape.java
@@ -8,4 +8,15 @@ public class IShape extends TetrisBlock {
         super(new int[][] {{1, 1, 1, 1}});
     }
 
+    @Override
+    public void rotate() {
+        super.rotate();
+        if (getWidth() == 1) {
+            setX(getX() + 1);
+            setY(getY() - 1);
+        } else {
+            setX(getX() - 1);
+            setY(getY() + 1);
+        }
+    }
 }
