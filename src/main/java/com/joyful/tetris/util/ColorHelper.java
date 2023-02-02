1eb6e9f8e3ec24e2ae6c7aab4f44123cb759195e
Александр Плохой <alexplohoy@mail.ru>
2023-02-02 16:17:01 +0300

    feat(view): new colors

--- src/main/java/com/joyful/tetris/util/ColorHelper.java
+++ src/main/java/com/joyful/tetris/util/ColorHelper.java
@@ -1,15 +1,19 @@
 package com.joyful.tetris.util;
 
 import java.awt.Color;
-import static java.awt.Color.BLUE;
-import static java.awt.Color.GREEN;
-import static java.awt.Color.RED;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 public final class ColorHelper {
-    private static final List<Color> AVAILABLE_COLORS = Arrays.asList(GREEN, RED, BLUE);
+    private static final List<Color> AVAILABLE_COLORS = Arrays.asList(
+            new Color(0, 100, 200), 
+            new Color(50, 180, 100), 
+            new Color(50, 200, 200), 
+            new Color(255, 255, 150), 
+            new Color(255, 155, 50), 
+            new Color(200, 100, 100)
+    );
 
     public static Color getNewColor(Color previousColor) {
         Color color;
